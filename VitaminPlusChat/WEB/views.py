f3682ca520b90d3895418cfc4271550e5d95f5f0
subin97 <s979769@icloud.com>
2021-08-09 20:03:06 +0900

    Revert "Revert "Revert "Revert "#210807 단어구름 시작""""
    This reverts commit 98f35f19e813ed81c3fa8642bfbc613b275aaa99.

--- VitaminPlusChat/WEB/views.py
+++ VitaminPlusChat/WEB/views.py
@@ -8,3 +8,6 @@ from django.shortcuts import redirect, render
 
 def index_home(request):
     return render(request, 'frontend/index.html')
+
+def keyword(request):
+    return render(request, 'keyword/keyword.html')
